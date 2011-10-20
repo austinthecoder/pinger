e27b3b108fdbed5700f0b32e810e7ab1589437e6
Austin Schneider <soccer022483@gmail.com>
2011-10-20 17:24:09 -0400

    undo premature refactoring out Pingable module

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,9 +1,9 @@
 class Location < ActiveRecord::Base
 
   extend Paginates
-  include Pingable
 
   has_many :alerts
+  has_many :pings
 
   validates :seconds,
     :presence => true,
@@ -17,12 +17,32 @@ class Location < ActiveRecord::Base
     }
   validates :title, :presence => true
 
+  class << self
+    # if there's no scheduled pings, schedule one
+    def schedule_pings!
+      all.each { |l| l.schedule_ping! unless l.pings.first_scheduled }
+    end
+  end
+
   def perform_request
     HTTParty.send http_method, url
   end
 
   def http_method=(value)
     self[:http_method] = value ? value.to_s.downcase : nil
+  end
+
+  def schedule_ping!
+    last_ping_at = pings.order { performed_at.desc }.first.try :performed_at
+    next_ping_to_schedule.schedule! ((last_ping_at || Time.now) + seconds)
+  end
+
+  def next_ping_to_schedule
+    pings.first_scheduled || pings.new
+  end
+
+  def next_ping_date
+    pings.first_scheduled.try :perform_at
   end
 
 end
